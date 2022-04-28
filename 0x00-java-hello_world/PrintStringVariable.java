013051e54839507df9bd55a18ca4096967cde668
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-04-28 15:13:47 -0300

    substring

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -1,7 +1,7 @@
 public class PrintStringVariable {
     public static void main(String[] args) {
         String texto = "Holberton School";
-        System.out.println(texto + "/n" + texto);
+        System.out.println(texto + "\n" + texto);
         System.out.println(texto.substring(0,9));
     }
 }
