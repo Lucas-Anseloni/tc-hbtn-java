6c503022ea4a6b696c9faa1fb41d5df2476f80e6
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-04-28 19:35:01 -0300

    teste

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -1,7 +1,12 @@
 public class Numeros0a99 {
     public static void main(String[] args) {
         for (int i =0;i<100;i++){
-            System.out.print(i + ", ");
+
+            if (i != 99){
+                System.out.print(i + ", ");
+            }else {
+                System.out.print(i);
+            }
         }
     }
 }
