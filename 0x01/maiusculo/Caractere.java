1c3c2375c1b03bc4dbe8f7bb5d7741d0ae6e5bb9
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-04-29 08:34:52 -0300

    somar

--- 0x01/maiusculo/Caractere.java
+++ 0x01/maiusculo/Caractere.java
@@ -1,18 +1,21 @@
+package maiusculo;
+
 public class Caractere {
     public static void main(String[] args) {
-        System.out.println(EhMaiusculo('C'));
 
+        boolean caractereEhMaiusculo1 = Caractere.EhMaiusculo('a');
+        boolean caractereEhMaiusculo2 = Caractere.EhMaiusculo('B');
+        boolean caractereEhMaiusculo3 = Caractere.EhMaiusculo('d');
+        boolean caractereEhMaiusculo4 = Caractere.EhMaiusculo('f');
+        boolean caractereEhMaiusculo5 = Caractere.EhMaiusculo('G');
 
     }
+
     public static boolean EhMaiusculo(char c){
-       while(c != 'C'){
-
-
-           System.out.println(false);
-           }
-
+        //if (c == charAt)
         return true;
     }
+
 
 
     }
