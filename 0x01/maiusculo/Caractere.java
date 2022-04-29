74e9278851f393ffbe068c43d2fc7d4f1eed48b8
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-04-28 23:12:19 -0300

    Caractere Maiusculo

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,10 @@
+public class Caractere {
+    public static void main(String[] args) {
+        System.out.println(EhMaiusculo('C'));
+
+
+    }
+    public static boolean EhMaiusculo(char c){
+        return true;
+    }
+}
