5cfba4fba65e87c1630f8b5061e5e4b8a95e23ff
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-04-28 22:21:23 -0300

    numeros

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -2,10 +2,10 @@ public class Numeros0a99 {
     public static void main(String[] args) {
         for (int i =0;i<100;i++){
 
-            if (i != 99){
+            if (i < 99){
                 System.out.print(i + ", ");
             }else {
-                System.out.print(i + " ");
+                System.out.print(i);
             }
         }
     }
