c7b47a76f67fa29cb71322beeda8ca04a54080f0
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-04-29 10:28:23 -0300

    somar

---
+++ 0x01/somar/Program.java
@@ -0,0 +1,11 @@
+package somar;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println(Numero.somar(24,32));
+        System.out.println(Numero.somar(5,0));
+        System.out.println(Numero.somar(4,-2));
+
+    }
+    }
+
