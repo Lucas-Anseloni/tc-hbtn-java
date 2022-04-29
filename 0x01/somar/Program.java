0c66b988d84085805fb4f0f64a349238a013eea2
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-04-29 10:34:29 -0300

    somar

--- 0x01/somar/Program.java
+++ 0x01/somar/Program.java
@@ -4,7 +4,7 @@ public class Program {
     public static void main(String[] args) {
         System.out.println(Numero.somar(24,32));
         System.out.println(Numero.somar(5,0));
-        System.out.println(Numero.somar(4,-2));
+        System.out.print(Numero.somar(4,-2));
 
     }
     }
