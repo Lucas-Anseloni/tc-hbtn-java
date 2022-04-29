b787fcfe7a9c8ebc3ac3e9259a4717549aae3311
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-04-29 10:26:39 -0300

    somar

--- 0x01/somar/Numero.java
+++ 0x01/somar/Numero.java
@@ -1,12 +1,10 @@
 package somar;
 
 public class Numero {
-    public static void main(String[] args) {
-        System.out.println(Numero.somar(1,2));
 
+
+        public static int somar ( int a, int b){
+            return (a + b);
         }
-    public static int somar(int a, int b){
-        return (a + b);
-    }
     }
 
