1e778246be4e53858ac950f6c300c672612d45dd
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-05-02 15:37:30 -0300

    exceção quadrado

--- 0x02/quadrado/Quadrado.java
+++ 0x02/quadrado/Quadrado.java
@@ -1,14 +1,11 @@
+package quadrado;
 public class Quadrado {
     public static double area(double lado) {
 
-        try {
+
             if (lado < 0) {
                 throw new IllegalArgumentException("Lado deve possuir valor positivo");
-
             }
-        } catch (Exception e) {
-            throw new IllegalArgumentException(e.getMessage());
-        }
         return lado * lado;
     }
 }
