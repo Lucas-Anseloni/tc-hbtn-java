f75c805d58fc067f48db0bdd5e84fdbc51b1846c
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-05-02 12:10:54 -0300

    exceção dividir

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -1,16 +1,17 @@
 public class Numero {
     public static void dividir(int a, int b){
+        int c = 0;
         try{
-            int c;
+
           c = a / b;
-            System.out.println(a + " / " + b + " = " + c);
+
           if (b == 0){
               throw new Exception("Nao eh permitido fazer uma divisao por zero!");
           }
         }catch (Exception e){
             System.out.println("Nao eh possivel dividir por zero");
         }finally {
-            System.out.println();
+            System.out.println(a + " / " + b + " = " + c);
         }
     }
 }
