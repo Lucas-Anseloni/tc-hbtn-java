af02bfbb0be966b4a272d0f82079d52e7cff2eb3
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-05-03 16:36:17 -0300

    exceptions conta especiais

--- 0x03/contas_especiais/ContaBancariaTarifada.java
+++ 0x03/contas_especiais/ContaBancariaTarifada.java
@@ -1,36 +1,36 @@
+
 import exceptions.OperacaoInvalidaException;
 
 public class ContaBancariaTarifada extends ContaBancariaBasica {
 
     private int quantidadeTransacoes;
 
-    public ContaBancariaTarifada(String numeracao, double taxaJurosAnual, double saldo) {
+    public ContaBancariaTarifada(String numeracao, double taxaJurosAnual) {
         super(numeracao, taxaJurosAnual);
-        // TODO Auto-generated constructor stub
+        this.quantidadeTransacoes = quantidadeTransacoes;
     }
 
-    @Override
-    void depositar(double valor) throws OperacaoInvalidaException {
-        if(valor > 0) {
-            this.saldo = saldo + valor - 0.10;
-        }else {
-            throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
-        }
-    }
     public int getQuantidadeTransacoes() {
         return quantidadeTransacoes;
     }
-    @Override
-    void sacar(double valor) throws OperacaoInvalidaException {
 
-        if (valor < 0) {
-            throw new OperacaoInvalidaException("Valor de saque deve ser maior que 0");
-        } else if (saldo < valor) {
-            throw new OperacaoInvalidaException("Valor de saque deve ser maior que o saldo atual");
-        } else {
-            saldo = saldo - valor - 0.10;
+    @Override
+    public void depositar(double valor) throws OperacaoInvalidaException {
+          if (valor <= 0){
+                throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
+            }else {
+                this.saldo = saldo + valor - 0.10;
+            }
         }
 
+    @Override
+    public void sacar(double valor) throws OperacaoInvalidaException {
+        if (valor < 0){
+            throw new conta_basica.exceptions.OperacaoInvalidaException("Valor de saque deve ser menor que 0");
+        }else if (valor > saldo){
+            throw new conta_basica.exceptions.OperacaoInvalidaException("Valor de saque deve ser menor que o saldo atual");
+        }else {
+            this.saldo = saldo - valor - 0.10;
+        }
     }
-
 }
