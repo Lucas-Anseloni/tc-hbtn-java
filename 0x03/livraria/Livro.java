f231229b5f682557329dc0354ee79975ca05fbb9
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-05-03 10:05:01 -0300

    exceptions livraria

---
+++ 0x03/livraria/Livro.java
@@ -0,0 +1,40 @@
+
+public class Livro {
+
+    String titulo;
+    String autor;
+    double preco;
+
+    public Livro() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Livro(String titulo, String autor, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
