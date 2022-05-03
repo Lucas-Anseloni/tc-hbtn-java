a05af3915a589461943b4ddd7c105a8f3c73c25f
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-05-03 16:05:19 -0300

    exceptions conta especiais

---
+++ 0x03/contas_especiais/ContaBancariaTarifada.java
@@ -0,0 +1,39 @@
+
+
+
+import contas_especiais.exceptions.OperacaoInvalidaException;
+
+
+public class ContaBancariaTarifada extends ContaBancariaBasica {
+
+    private int quantidadeTransacoes;
+
+    public ContaBancariaTarifada(String numeracao, double taxaJurosAnual) {
+        super(numeracao, taxaJurosAnual);
+        this.quantidadeTransacoes = quantidadeTransacoes;
+    }
+
+    public int getQuantidadeTransacoes() {
+        return quantidadeTransacoes;
+    }
+
+    @Override
+    public void depositar(double valor) throws OperacaoInvalidaException {
+          if (valor <= 0){
+                throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
+            }else {
+                this.saldo = saldo + valor - 0.10;
+            }
+        }
+
+    @Override
+    public void sacar(double valor) throws conta_basica.exceptions.OperacaoInvalidaException {
+        if (valor < 0){
+            throw new conta_basica.exceptions.OperacaoInvalidaException("Valor de saque deve ser menor que 0");
+        }else if (valor > saldo){
+            throw new conta_basica.exceptions.OperacaoInvalidaException("Valor de saque deve ser menor que o saldo atual");
+        }else {
+            this.saldo = saldo - valor - 0.10;
+        }
+    }
+}
