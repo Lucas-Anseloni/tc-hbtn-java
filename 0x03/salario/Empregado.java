1785ce36d6caa68b85041e54646899a71ee730c8
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-05-02 22:06:08 -0300

    salario

--- 0x03/salario/Empregado.java
+++ 0x03/salario/Empregado.java
@@ -11,14 +11,14 @@ public class Empregado {
 
     double calcularBonus(Departamento departamento){
         if(departamento.alcancouMeta() == true){
-            return salarioFixo * 0.1;
+            return salarioFixo * 0.1d;
         }
         return 0;
     }
 
     double calcularSalarioTotal(Departamento departamento){
         if(departamento.alcancouMeta() == true){
-            return salarioFixo * 1.1;
+            return salarioFixo * 1.1d;
         }
         return salarioFixo;
     }
--- 0x03/salario/Gerente.java
+++ 0x03/salario/Gerente.java
@@ -7,14 +7,14 @@ public class Gerente extends Empregado{
     
     double calcularBonus(Departamento departamento){
         if(departamento.alcancouMeta() == true){
-            return ((salarioFixo * 0.2) + (departamento.valorAtingidoMeta - departamento.valorMeta) * 0.01);
+            return ((salarioFixo * 0.2d) + (departamento.valorAtingidoMeta - departamento.valorMeta) * 0.01d);
         }
         return 0;
     }
 
     double calcularSalarioTotal(Departamento departamento){
         if(departamento.alcancouMeta() == true){
-            return  salarioFixo + ((salarioFixo * 0.2) + (departamento.valorAtingidoMeta - departamento.valorMeta) * 0.01);
+            return  salarioFixo + ((salarioFixo * 0.2d) + (departamento.valorAtingidoMeta - departamento.valorMeta) * 0.01d);
         }
         return salarioFixo;
     }
