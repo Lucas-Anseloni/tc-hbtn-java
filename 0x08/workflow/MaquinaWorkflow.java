5ee192bb90e21007618298e8d00d627a5c720c66
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-05-09 09:27:50 -0300

    Interface fixação

---
+++ 0x08/workflow/MaquinaWorkflow.java
@@ -0,0 +1,12 @@
+package workflow;
+
+import atividades.Atividade;
+
+public class MaquinaWorkflow {
+
+    public void executar(Workflow workflow) {
+        for (Atividade atividade: workflow.getAtividades()) {
+            atividade.executar();
+        }
+    }
+}
