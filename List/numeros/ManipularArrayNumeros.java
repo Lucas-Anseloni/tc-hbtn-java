7dcd95ea7327a28de44b56195bae11e290a18a9c
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-05-17 08:50:34 -0300

    List manipular numeros arrayList

--- List/numeros/ManipularArrayNumeros.java
+++ List/numeros/ManipularArrayNumeros.java
@@ -1,6 +1,5 @@
 
 
-
 import java.util.List;
 
 public class ManipularArrayNumeros {
@@ -13,28 +12,28 @@ public class ManipularArrayNumeros {
         }
             return -1;
     }
-    public static void adicionarNumero(List<Integer> numeros, int i){
+    public static void adicionarNumero(List<Integer> numeros, int i) {
         if (buscarPosicaoNumero(numeros, i) != -1){
-            numeros.add(i);
-        }else {
             throw new IllegalArgumentException("Numero jah contido na lista");
+        }else {
+            numeros.add(i);
         }
 
     }
 
     public static void removerNumero(List<Integer> numeros, int i) {
-        if (buscarPosicaoNumero(numeros, i) != -1){
-            numeros.remove(buscarPosicaoNumero(numeros, i));
-        }else {
+        if (buscarPosicaoNumero(numeros, i) == -1){
             throw new IllegalArgumentException("Numero nao encontrado na lista");
+        }else {
+            numeros.remove(buscarPosicaoNumero(numeros, i));
         }
     }
 
-    public static void substituirNumero(List<Integer> numeros, int numeroSubstituir, int numeroSubstituto) {
-        if (numeros.contains(numeroSubstituir)){
-            numeros.set(buscarPosicaoNumero(numeros,numeroSubstituir),numeroSubstituto);
-        }else {
-            numeros.add(numeroSubstituto);
+    public static void substituirNumero(List<Integer> numeros, int i, int i1) {
+        if (buscarPosicaoNumero(numeros,i) == -1){
+            numeros.add(i1);
+            }else {
+            numeros.set(buscarPosicaoNumero(numeros, i),i1);
         }
     }
 }
